f3919d10f17d1649807a048bdb27e83b70e42407
rMaloberti <r.maloberti@yahoo.com>
2022-06-09 19:27:32 +0200

    feat(recipes): create the recipe cards rendering script

--- scripts/Views/Components.js
+++ scripts/Views/Components.js
@@ -127,4 +127,115 @@ export default class Components {
 
     return filtersBtn;
   };
+
+  // RECIPE CARD
+  static recipeCard = (data) => {
+    /* Component datas */
+    // eslint-disable-next-line object-curly-newline
+    const { recipeId, recipeTitle, recipeTime, recipeIngredients, recipeDesc } = data;
+
+    /* Recipe card */
+    const recipeCard = document.createElement('div');
+    recipeCard.id = `recipe-${recipeId}`;
+    recipeCard.classList.add('recipe-card');
+
+    /* Card thumbnail */
+    const cardThumb = document.createElement('div');
+    cardThumb.classList.add('recipe-card-thumbnail');
+
+    /* Thumbnail image */
+    const thumbImg = document.createElement('img');
+    thumbImg.classList.add('recipe-card-thumbnail__img');
+    thumbImg.setAttribute('alt', '');
+    thumbImg.setAttribute('src', '');
+    /* END Thumbnail image */
+
+    cardThumb.appendChild(thumbImg);
+    /* END Card thumbnail */
+
+    /* Card description */
+    const cardDesc = document.createElement('div');
+    cardDesc.classList.add('recipe-card-desc');
+
+    /* Description header */
+    const descHeader = document.createElement('div');
+    descHeader.classList.add('card-desc-header');
+
+    /* Description heading */
+    const descHeading = document.createElement('h2');
+    descHeading.classList.add('card-desc-header__heading');
+    descHeading.textContent = recipeTitle;
+    /* END Description heading */
+
+    /* Description cooking time */
+    const descCookTime = document.createElement('div');
+    descCookTime.classList.add('desc-header-cooking-time');
+
+    /* Cooking time icon */
+    const cookTimeIcon = document.createElement('i');
+    cookTimeIcon.classList.add('desc-header-cooking-time__icon');
+    /* END Cooking time icon */
+
+    /* Cooking time text */
+    const cookTimeTxt = document.createElement('p');
+    cookTimeTxt.classList.add('desc-header-cooking-time__text');
+    cookTimeTxt.textContent = `${recipeTime} min`;
+
+    descCookTime.appendChild(cookTimeIcon);
+    descCookTime.appendChild(cookTimeTxt);
+    /* END Description cooking time */
+
+    descHeader.appendChild(descHeading);
+    descHeader.appendChild(descCookTime);
+    /* END Description header */
+
+    /* Description body */
+    const descBody = document.createElement('div');
+    descBody.classList.add('card-desc-body');
+
+    /* Description ingredients */
+    const descIngredients = document.createElement('ul');
+    descIngredients.classList.add('desc-body-ingredients');
+
+    recipeIngredients.forEach((ingredient) => {
+      /* List ingredient */
+      const listIngredient = document.createElement('li');
+      listIngredient.classList.add('desc-body-ingredient');
+      listIngredient.textContent = `${ingredient.ingredient}${ingredient.quantity || ingredient.quantite ? ': ' : ''}`;
+
+      if (ingredient.quantity || ingredient.quantite) {
+        /* Ingredient quantity */
+        const ingredientQty = document.createElement('span');
+        ingredientQty.classList.add('desc-body-ingredient__qty');
+        ingredientQty.textContent = `${ingredient.quantity || ingredient.quantite}${ingredient.unit ? ` ${ingredient.unit}` : ''}`;
+        /* END Ingredient quantity */
+
+        listIngredient.appendChild(ingredientQty);
+      }
+      /* END List ingredient */
+
+      descIngredients.appendChild(listIngredient);
+    });
+    /* END Description ingredients */
+
+    /* Description instructions */
+    const descInstructions = document.createElement('p');
+    descInstructions.classList.add('card-desc-body__instructions');
+    descInstructions.textContent = recipeDesc;
+    /* END Description instructions */
+
+    descBody.appendChild(descIngredients);
+    descBody.appendChild(descInstructions);
+    /* END Description body */
+
+    cardDesc.appendChild(descHeader);
+    cardDesc.appendChild(descBody);
+    /* END Card description */
+
+    recipeCard.appendChild(cardThumb);
+    recipeCard.appendChild(cardDesc);
+    /* END Component wrapper */
+
+    return recipeCard;
+  };
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable import/extensions */
 import Components from './views/Components.js';
 
@@ -11,6 +12,42 @@ const init = () => {
   document
     .getElementById('filters')
     .appendChild(Components.filtersBtn({ type: 'tools', filters: ['test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test'] }));
+
+  for (let i = 0; i < 10; i += 1) {
+    document.getElementById('recipes').appendChild(
+      Components.recipeCard({
+        recipeId: 1,
+        recipeTitle: 'Limonade de Coco',
+        recipeTime: 10,
+        recipeIngredients: [
+          {
+            ingredient: 'Lait de coco',
+            quantity: 400,
+            unit: 'ml',
+          },
+          {
+            ingredient: 'Jus de citron',
+            quantity: 2,
+          },
+          {
+            ingredient: 'Crème de coco',
+            quantity: 2,
+            unit: 'cuillères à soupe',
+          },
+          {
+            ingredient: 'Sucre',
+            quantite: 30,
+            unit: 'grammes',
+          },
+          {
+            ingredient: 'Glaçons',
+          },
+        ],
+        recipeDesc:
+          "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée",
+      })
+    );
+  }
 };
 
 init();
