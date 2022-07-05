47d11994cec496b9fd3b7e623451bb716aa13f44
rMaloberti <r.maloberti@yahoo.com>
2022-07-05 14:07:11 +0200

    feat(models): restart the getReferences method from scratch

--- scripts/models/RecipesModel.js
+++ scripts/models/RecipesModel.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/extensions */
 import recipes from '../../data/recipes.js';
-import SearchHelper from '../utils/SearchHelper.js';
 
 // RECIPES MODEL
 export default class RecipesModel {
@@ -12,34 +11,6 @@ export default class RecipesModel {
     return recipesArr;
   };
 
-  // GET FILTER REFERENCES
-  static getFilterReferences = (recipesArr) => {
-    // Filters array
-    const filters = [[], [], []];
-
-    // For each recipe
-    recipesArr.forEach((recipe) => {
-      // For each ingredient
-      recipe.ingredients.forEach((ingr) => {
-        // Add the ingredient to the filters array
-        filters[0].push(SearchHelper.normalize(ingr.ingredient));
-      });
-      // Add the applience to the filters array
-      filters[1].push(SearchHelper.normalize(recipe.appliance));
-      // For each ustensil
-      recipe.ustensils.forEach((ustensil) => {
-        // Add the ustensil to the filters array
-        filters[2].push(SearchHelper.normalize(ustensil));
-      });
-    });
-
-    // Filters set == Filters array without repetition
-    const filtersSet = [
-      [...new Set(filters[0])],
-      [...new Set(filters[1])],
-      [...new Set(filters[2])],
-    ];
-
-    return filtersSet;
-  };
+  // GET REFERENCES
+  static getReferences = () => {};
 }
